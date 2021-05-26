b0f48c1b011904cd5bdd5fe97adf159c0be14c11
VarunKrishnanV <VarunKrishnanV>
2021-05-27 03:27:14 +0530

    header color was customized

--- js/aboutFileManager.js
+++ js/aboutFileManager.js
@@ -10,12 +10,18 @@ quickArrow.addEventListener("click", ()=>{
 aboutDoc.addEventListener("click", () => {
     const aboutContent = new WinBox({
         title : "about.txt",
-        background : "#4eb5fa",
+        background : "#0567D4",
         url : "/pages/about.html",
         // x: "top",
         // y: "right",
         width: "80%",
-        height: "80%"
+        height: "80%",
+        onfocus: function(){
+            this.setBackground("#0567D4");
+        },
+        onblur: function(){
+            this.setBackground("#00B4FB");
+        },
     })
 })
 
--- js/script.js
+++ js/script.js
@@ -3,14 +3,20 @@ var folderAbout = document.querySelector(".folder-about")
 folderAbout.addEventListener("click", () => {
     const aboutContent = new WinBox({
         title : "About Me",
-        background : "#4eb5fa",
+        background : "#0567D4",
         url : "/pages/aboutFileManager.html",
         // x: "center",
         // y: "center",
         // width: "80%",
         // height: "50%"
         width: "90%",
-        height: "80%"
+        height: "80%",
+        onfocus: function(){
+            this.setBackground("#0567D4");
+        },
+        onblur: function(){
+            this.setBackground("#00B4FB");
+        },
     })
 })
 
